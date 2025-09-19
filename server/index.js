6ccf9e017a9ee5cbfd1ee0f66514034c73df8f84
Prabhjot Singh <lazycoder001@gmail.com>
2025-09-19 22:20:32 +0530

    setup basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log("Hello Swiggy");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is UP and RUNNING on port: ${PORT}`);
+});
+
+app.get("/", (req, res)=>{
+    res.send("Test route")
+})
