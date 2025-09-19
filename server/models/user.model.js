e9c0b5aaceacc609f6eb074158a1751a734ac0fe
Prabhjot Singh <lazycoder001@gmail.com>
2025-09-19 23:49:34 +0530

    created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    mobile: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "merchant", "deliveryAgent"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
